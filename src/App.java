d9c73ef41119661c5d5c24c86861961fa280bc2e
Benjamin Bitterling <122120014+Benjamin-Bitterling@users.noreply.github.com>
2024-04-05 14:04:11 -0500

    Snake2.0.6

--- src/App.java
+++ src/App.java
@@ -1,20 +1,26 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class App {
     public static void main(String[] args) {
 
         // Inits
-        int boardWidth = 600;
-        int boardHeight = boardWidth;
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int deviceWidth = (int) Math.round(screenSize.getWidth());
+        int deviceHeight = (int) Math.round(screenSize.getHeight());
+
+        System.out.println(deviceWidth + ", " + deviceHeight);
 
         JFrame frame = new JFrame("Snake");
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.setUndecorated(true);
         frame.setVisible(true);
-        frame.setSize(boardWidth, boardHeight);
-        frame.setLocation(660, 240);
+        frame.setSize(deviceWidth, deviceHeight);
+        frame.setLocation(0, 0);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        SnakeGame snakeGame = new SnakeGame(boardWidth, boardHeight);
+        SnakeGame snakeGame = new SnakeGame(deviceWidth, deviceHeight);
         frame.add(snakeGame);
         frame.pack();
         snakeGame.requestFocus();
